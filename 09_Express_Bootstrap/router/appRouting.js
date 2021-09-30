f8361f6c3d917f02a9456b821067e77afb940b54
RAHUL ROY <rahulkumarvivoy51l@gmail.com>
2021-09-30 18:18:11 +0530

    exp_Bootstrap

---
+++ 09_Express_Bootstrap/router/appRouting.js
@@ -0,0 +1,31 @@
+const path = require("path");
+let mapRoutes=function(app){
+  //Express Routing
+  //home page
+ app.get('/',(request,response)=>{
+        response.sendFile(path.join(__dirname,'..','views','index.html'));
+    });
+   //about page
+    app.get('/about',(request,response)=>{
+        response.sendFile(path.join(__dirname,'..','views','about.html'));
+    });
+    //services page
+    app.get('/services',(request,response)=>{
+        response.sendFile(path.join(__dirname,'..','views','services.html'));
+    });
+    //profile page
+    app.get('/profile',(request,response)=>{
+        response.sendFile(path.join(__dirname,'..','views','profile.html'));
+    });
+    //contact page
+    app.get('/contact',(request,response)=>{
+        response.sendFile(path.join(__dirname,'..','views','contact.html'));
+    });
+    //404 page
+    app.use((request,response)=>{
+        response.sendFile(path.join(__dirname,'..','views','404.html'));
+    });
+};
+module.exports={
+    mapRoutes
+};
