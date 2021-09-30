38d32a76bb56978219914fd95a143b4f7e4ac3c1
RAHUL ROY <rahulkumarvivoy51l@gmail.com>
2021-09-30 18:25:35 +0530

    NODE_JS_Basic

---
+++ 01_NODE_js_Basic/app.js
@@ -0,0 +1,19 @@
+///basic example
+console.log("welcome to node js");
+//object
+let student={
+  name:'Rahul',
+  age:21,
+    occupation:'Software Developer'
+};
+console.log(student);
+//array
+let colors=['black','pink','red','white'];
+console.log(colors);
+//function
+let greetMe=(name)=>{
+    console.log(`Hello ${name} good morning`);
+
+};
+greetMe("john");
+//
